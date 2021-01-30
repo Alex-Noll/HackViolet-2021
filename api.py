9e4e966a779165e81f7e13cd9244bd629786d9be
ceholmes <56744477+ceholmes@users.noreply.github.com>
2021-01-30 16:05:30 -0500

    Update api.py
    Changed search function so it searched based on keywords instead of IDs

--- api.py
+++ api.py
@@ -7,8 +7,8 @@ app.config["DEBUG"] = True
 recipes = [
     {'id': 0,
      'title': 'Gluten Free Waffles',
-     'ingredients': ['eggs', 'baking flour(gluten free)',
-                     'baking soda', 'salt', 'sugar', 'vanilla extract', 'butter', 'milk'],
+     'ingredients': ['eggs', 'baking flour(gluten free)', 'baking soda',
+                     'salt', 'sugar', 'vanilla extract', 'butter', 'milk'],
      'tags': ['gluten free', 'vegetarian'],
      'url': 'https://www.pinterest.com/pin/5559199530451854/'
     },
@@ -25,10 +25,12 @@ recipes = [
      'title': 'Walnut and Lentil Bolognese',
      'ingredients': ['olive oil', 'carrots', 'celery', 'onion', 'walnuts',
                      'lentils', 'marinara sauce', 'pasta'],
+     'tags': ['vegan'],
      'url': 'https://themodernproper.com/walnut-and-lentil-bolognese'
     }]
 
 
+#Followed tutorial from https://programminghistorian.org/en/lessons/creating-apis-with-python-and-flask
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Recipe Search</h1><p>This site is a prototype API for a recipe searching program.</p>"
@@ -39,24 +41,24 @@ def api_all():
 
 @app.route('/api/v1/resources/recipes', methods=['GET'])
 def api_id():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
-    if 'id' in request.args:
-        id = int(request.args['id'])
+    # Check if a tag was provided as part of the URL.
+    # If tag is provided, assign it to a variable.
+    # If no ID is provided, display all recipes.
+    if 'tag' in request.args:
+        tag = request.args['tag']
     else:
-        return "Error: No id field provided. Please specify an id."
+        return jsonify(recipes)
 
-    # Create an empty list for our results
+    # Create an empty list for results
     results = []
 
-    # Loop through the data and match results that fit the requested ID.
-    # IDs are unique, but other fields might return many results
+    # Loop through the data and match results that fit the requested tag.
     for recipe in recipes:
-        if recipe['id'] == id:
+        print(recipe.get('tags'))
+        if tag in recipe.get('tags'):
             results.append(recipe)
 
-    # Use the jsonify function from Flask to convert our list of
+    # Use the jsonify function from Flask to convert list of
     # Python dictionaries to the JSON format.
     return jsonify(results)
 
